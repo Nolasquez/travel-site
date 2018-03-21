889531b8c816d01c9caac59d2ba2c985118d4da7
nolasquez <ialdonolasquez@me.com>
2018-03-20 22:30:48 -0600

    Re-organize structure

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch')
+gulp.task('default', function()
+{
+	console.log("Soy la verga")
+}
+);
+gulp.task('html', function(){
+	console.log("Aldo Nolasquez")
+})
+
+gulp.task('watch', function(){
+	watch('', '');
+})
